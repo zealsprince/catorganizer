11533571aa93e848daf39853e3997f367dcb3c78
Andrew Lake <andrew@zealsprince.com>
2023-12-31 17:43:03 +0100

    Add basic configuration reading

--- lib/src/models/manifest.dart
+++ lib/src/models/manifest.dart
@@ -1,14 +1,49 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+
+import 'package:catorganizer/src/constants.dart' as constants;
 import 'package:catorganizer/src/models/category.dart';
 import 'package:catorganizer/src/models/document.dart';
 import 'package:catorganizer/src/models/tag.dart';
+
+class ManifestDataModel {
+  final Map<String, CategoryModel> categories = {};
+  final Map<String, DocumentModel> documents = <String, DocumentModel>{};
+}
 
 class ManifestModel extends ChangeNotifier {
   final Map<String, CategoryModel> _categories = {};
   final Map<String, DocumentModel> _documents = <String, DocumentModel>{};
 
   Future<void> readManifest() async {
+    final directory = Directory.current.path; // Get the current execution path.
+
+    ManifestDataModel data = ManifestDataModel(); // Create a sanitized model.
+
+    File configurationFile =
+        File(path.join(directory, constants.manifestFileName));
+
+    if (await configurationFile.exists()) {
+      // Read the configuration file.
+      String contents = await configurationFile.readAsString();
+
+      data = jsonDecode(contents)
+          as ManifestDataModel; // Attempt to read as our sanitized Manifest data model.
+
+      _categories.addAll(data.categories);
+      _documents.addAll(data.documents);
+    } else {
+      // Create the configuration file.
+      configurationFile.create();
+
+      // Write the empty sanitized data model.
+      configurationFile.writeAsString(json.encode(data));
+    }
+
     notifyListeners();
   }
 
@@ -62,7 +97,7 @@ class ManifestModel extends ChangeNotifier {
   }
 
   ManifestModel() {
-    initializeDummy();
+    initialize();
   }
 
   ManifestModel.withDocuments(Map<String, DocumentModel> documents) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,14 @@ import Foundation
 
 import desktop_drop
 import file_selector_macos
+import path_provider_foundation
 import screen_retriever
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
